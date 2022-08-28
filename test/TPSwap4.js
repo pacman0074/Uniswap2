034b95a58eb412d081749ab389b600e53e6f2b72
yannis <yannis.ayouaz@gmail.com>
2022-08-29 01:30:07 +0200

    commit

--- test/TPSwap4.js
+++ test/TPSwap4.js
@@ -5,7 +5,7 @@ const ethers = require('ethers');
 
 
 module.exports = async function(done){
-    const DAI = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18)
+    const DAI = new Token(ChainId.MAINNET, '0xdAC17F958D2ee523a2206206994597C13D831ec7', 18)
     
     const pair = await Fetcher.fetchPairData(DAI, WETH[DAI.chainId])
     
