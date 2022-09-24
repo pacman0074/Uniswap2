c779339e5707b2bbe9ccaa39840740cb27dce125
yannis <yannis.ayouaz@gmail.com>
2022-09-24 12:13:43 +0200

    commit

--- test/TPSwap4.js
+++ test/TPSwap4.js
@@ -19,7 +19,7 @@ module.exports = async function(done){
     
     const amountOutMin = trade.minimumAmountOut(slippageTolerance).raw // needs to be converted to e.g. hex
     const path = [WETH[DAI.chainId].address, DAI.address]
-    const to = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8' // should be a checksummed recipient address
+    const to = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266' // should be a checksummed recipient address
     const deadline = Math.floor(Date.now() / 1000) + 60 * 20 // 20 minutes from the current Unix time
     const value = trade.inputAmount.raw // // needs to be converted to e.g. hex
     
